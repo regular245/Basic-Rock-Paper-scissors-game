c80a84b0f452efcadcc695f2f604cc6a4c05be83
regular245 <80796332+regular245@users.noreply.github.com>
2023-08-19 19:31:46 -0500

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,53 @@
+import random
+
+
+options=('rock', 'paper', 'scissors')
+running = True 
+while running:
+
+    player=None
+    computer=random.choice(options)
+
+    name=input('what is your name:')
+
+    comp=0
+    p=0
+    #f"{name}p =0"
+
+
+    while player not in options:
+        player=input('enter a choice(rock paper scissors):')
+
+
+    print(f'player: {player}')
+    print(f'computer: {computer}')
+
+    if player==computer:
+        print('its a tie!')
+        p=+1
+        comp=+1
+    elif player == 'rock' and computer == 'scissors':
+        print( f"{name} wins" )
+        p=+1
+    elif player == 'paper' and computer == 'rock':
+        print( f"{name} wins")
+        p=+1
+    elif player == 'scissors' and computer == 'paper':
+        print( f"{name} wins")
+        p=+1
+    else:
+        print('computer wins')
+        comp=+1
+
+    if not input("Play again? (y/n): ").lower() == "y":
+        running = False
+
+
+    print('the computer has',comp,'points')
+    print(f"{name} has", p, 'points')
+
+
+
+
+
+
